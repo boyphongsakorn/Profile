805c8c5550c2be14a79401dbf239a2b41fb33999
boyphongsakorn <boy1556@hotmail.com>
2018-06-29 06:40:23 +0000

    Replace footer.php

--- footer.php
+++ footer.php
@@ -3,10 +3,10 @@
   <hr>
     <div class="col-lg-12">
       <div class="col-md-8">
-        <a href="http://m.me/boyphongsakornnetwork">Contact</a> <!--a href="#">กฎ,ข้อตกลงและการใช้ข้อมูลจากFacebook (soon)</a-->    
+        <!--a href="http://m.me/boyphongsakornnetwork">Contact</a> <a href="#">กฎ,ข้อตกลงและการใช้ข้อมูลจากFacebook (soon)</a-->    
       </div>
       <div class="col-md-4">
-        <p class="muted pull-right">© 2017 BoyPhongsakorn.xyz All rights reserved</p>
+        <p class="muted pull-right">© 2017-2018 Quad B All rights reserved</p>
       </div>
     </div>
   </div>
